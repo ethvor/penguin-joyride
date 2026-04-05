f87294de6c7cb0b0596fe65450cd4b47a50e5cef
Ethan Voraritskul <contactethanv@gmail.com>
2026-04-05 19:56:01 -0400

    added penguin sprite and adjusted hitbox, unsaved changes from last commit
    added a penguin sprite and run animation (not yet implemented, but in the project) by SeanPatrick Pendleton. saved project and committed changes that should have been in the last commit.

--- Assets/Scripts/ParallaxLayer.cs
+++ Assets/Scripts/ParallaxLayer.cs
@@ -2,29 +2,31 @@ using UnityEngine;
 
 public class ParallaxLayer : MonoBehaviour
 {
-    public float speedMultiplier = 0.5f; // lower = farther away, scrolls slower
+    public float speedMultiplier; //lower = farther away, scrolls slower
 
-    private Material mat;
-    private Vector2 startOffset;
+    private Material mat;         //this object's material, grabbed in Start
+    private Vector2 startOffset;  //saves starting offset so we can reset it
 
     void Start()
     {
-        // material.mainTextureOffset slides the texture sideways without moving the object
-        // the texture needs Wrap Mode set to Repeat in the Inspector for this to loop seamlessly
-        mat = GetComponent<Renderer>().material;
-        startOffset = mat.mainTextureOffset;
+        //mainTextureOffset slides the texture sideways without moving game object
+        //texture wrap mode on repeat in the Inspector so it loops
+        mat = GetComponent<Renderer>().material; //Renderer is the component that draws this object
+        startOffset = mat.mainTextureOffset;   //get mat start offset so we can reset to it after death
 
-        GameManager.Instance.onReset.AddListener(ResetLayer);
+        GameManager.Instance.onReset.AddListener(ResetLayer); //snap back to start on reset
     }
 
+    // Update is called once per frame
     void Update()
     {
+        //shift texture left based on game scroll speed, scaled by this layer's multiplier
         float offset = GameManager.Instance.scrollSpeed * speedMultiplier * Time.deltaTime;
-        mat.mainTextureOffset += new Vector2(offset, 0);
+        mat.mainTextureOffset += new Vector2(offset, 0); //only moves horizontally
     }
 
     void ResetLayer()
     {
-        mat.mainTextureOffset = startOffset;
+        mat.mainTextureOffset = startOffset; //undo all scrolling
     }
 }
