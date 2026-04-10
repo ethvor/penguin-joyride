d79fd97b314f749a4b6083b96799e14f79fa9405
lianmang-cs <lianmang238@gmail.com>
2026-04-10 08:34:02 -0500

    Coin collision to player collision. Fixed coin audio issue.
    Moved the coin collision algorithm to player collision script. Fixed audio issue, where audio didn't play when colliding with the player.

--- Assets/Scripts/CoinBehaviour.cs
+++ Assets/Scripts/CoinBehaviour.cs
@@ -1,28 +1,8 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class CoinBehaviour : MonoBehaviour
 {
-    private int coinValue = 1;
-    public AudioSource coinAudio; 
-    public AudioClip coinClip;
-    void Start() {
-        coinAudio = GetComponent<AudioSource>(); 
-    }
+    public int coinValue = 1;
+    public AudioClip coinAudio; 
     
-    //Collision of player and coin
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(other.CompareTag("Player")) {
-            CoinManager coinmanager = other.GetComponent<CoinManager>(); 
-            if(coinmanager != null) {
-                coinmanager.AddCoins(coinValue); 
-            }
-            //Play audio
-            coinAudio.PlayOneShot(coinClip);
-            
-            //Destroy coin 
-            Destroy(gameObject); 
-        }
-
-    }
 }
--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
-{
-    
-    
-    
-    
+{     
     void OnTriggerEnter2D(Collider2D other) // on collision with 'trigger' enabled collider
     {
         if (other.transform.root.CompareTag("Hazard"))
         {
             GameManager.Instance.TakeDamage(GameManager.Instance.hazardDamage); //deal damage using game manager
+        }
+
+        //collision with player and coin
+        if(other.gameObject.tag == "Coin") {
+            CoinManager coinmanager = other.GetComponent<CoinManager>();
+            CoinBehaviour coin = other.GetComponent<CoinBehaviour>(); 
+
+            if(coinmanager != null) {
+                coinmanager.AddCoins(coin.coinValue); 
+            }
+            //Play audio
+            float volume = 1; 
+            AudioSource.PlayClipAtPoint(coin.coinAudio, other.transform.position, volume); 
+            
+            //Destroy coin 
+            Destroy(other.gameObject); 
         }
     }
 }
