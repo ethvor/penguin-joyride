7cbca87f1e3881113526f487ac071e7573edc2c2
JustinHo09 <hojustin09@gmail.com>
2026-04-09 20:05:21 -0500

    I created the animations for the player and coin.
    I made the animation for the coin and gave it a rigidbody and collider, and I made it a prefab to be used later. Then I fixed the sprite slicing done my unity and made the player animations. I also made the animations transition with the script.

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -19,7 +19,7 @@ public class PlayerBehavior : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         //multiplier = 10;
-        //anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         // subscribe to GameManager events
         GameManager.Instance.onGameOver.AddListener(dieVFX);
     }
@@ -29,21 +29,18 @@ public class PlayerBehavior : MonoBehaviour
     {
         if(Keyboard.current.upArrowKey.wasPressedThisFrame|| Keyboard.current.spaceKey.wasPressedThisFrame)
         {
+            anim.SetTrigger("fly");
             jp.Play();
         }
         
-        // if(Keyboard.current.upArrowKey.isPressed || Keyboard.current.spaceKey.isPressed)
-        // {
-        //     rb.gravityScale = 0.0f;
-        //     rb.linearVelocity = new Vector2(0.0f, 0.0f);
-        //     //rb.mass = 0.0f;
-        //     //float newY = transform.position.y + speed;
-        //     //transform.position = new Vector2(transform.position.x, newY);
-        //     rb.AddForceY(speed * Time.fixedDeltaTime,ForceMode2D.Impulse);
-        // }
+        if(Keyboard.current.downArrowKey.wasPressedThisFrame|| Keyboard.current.shiftKey.wasPressedThisFrame)
+        {
+            anim.SetTrigger("fall");
+        }
         
         if(Keyboard.current.upArrowKey.wasReleasedThisFrame || Keyboard.current.spaceKey.wasReleasedThisFrame)
         {
+            anim.SetTrigger("fall");
             rb.gravityScale = 0.5f;
             rb.linearVelocity = new Vector2(0.0f, 0.0f);
             jp.Stop();
@@ -54,15 +51,6 @@ public class PlayerBehavior : MonoBehaviour
             rb.gravityScale = 0.5f;
             rb.linearVelocity = new Vector2(0.0f, 0.0f);
         }
-
-
-        
-        // if(Keyboard.current.downArrowKey.isPressed || Keyboard.current.shiftKey.isPressed)
-        // {
-        //     //rb.gravityScale += 0.5f;
-        //     rb.gravityScale = 0.0f;
-        //     rb.AddForceY(-down * Time.fixedDeltaTime,ForceMode2D.Impulse);
-        // }
     }
 
     public void FixedUpdate()
@@ -71,20 +59,23 @@ public class PlayerBehavior : MonoBehaviour
         {
             rb.gravityScale = 0.0f;
             rb.linearVelocity = new Vector2(0.0f, 0.0f);
-            //rb.mass = 0.0f;
-            //float newY = transform.position.y + speed;
-            //transform.position = new Vector2(transform.position.x, newY);
             rb.AddForceY(speed * Time.fixedDeltaTime,ForceMode2D.Impulse);
         }
         
         if(Keyboard.current.downArrowKey.isPressed || Keyboard.current.shiftKey.isPressed)
         {
-            //rb.gravityScale += 0.5f;
             rb.gravityScale = 0.0f;
             rb.AddForceY(-down * Time.fixedDeltaTime,ForceMode2D.Impulse);
         }
     }
- 
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("floor"))
+        {
+            anim.SetTrigger("run");
+        }
+    }
 
     void dieVFX()
     {
