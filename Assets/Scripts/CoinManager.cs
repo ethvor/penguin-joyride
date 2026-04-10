73996dcd840250551b3f5006ef4a332cf52a8da9
lianmang-cs <lianmang238@gmail.com>
2026-04-10 13:58:53 -0500

    Added scoreText

--- Assets/Scripts/CoinManager.cs
+++ Assets/Scripts/CoinManager.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
+using TMPro; 
 
 public class CoinManager : MonoBehaviour
 {
     public int totalCoins; 
     private int currMultiplier;
-    private float multiplierTimer;  
+    private float multiplierTimer; 
+    public TMP_Text scoreText;  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         totalCoins = 0; 
         currMultiplier = 1; 
         multiplierTimer = 0f; 
+        updateScore(); 
     }
 
     // Update is called once per frame
@@ -25,11 +28,17 @@ public class CoinManager : MonoBehaviour
         
     }
     public void AddCoins(int amount) {
-        totalCoins += amount; 
+        totalCoins += amount * currMultiplier;
+        updateScore(); 
     }
     public void ActivateMultiplier(int multiplier, float duration) {
         currMultiplier = multiplier; 
         multiplierTimer = duration; 
+    }
+    public void updateScore() {
+        if(scoreText != null) {
+            scoreText.SetText("Coins: " + totalCoins); 
+        }
     }
 
 }
