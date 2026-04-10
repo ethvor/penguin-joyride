fe19deda3b2655f32f3dbeca010a4f27356e0192
lianmang-cs <lianmang238@gmail.com>
2026-04-09 21:36:00 -0500

    Made the CoinBehaviour and CoinManager Scripts.
    For now, the CoinBehaviour script controls the collision detection. The CoinManager keeps track of the coins collected by player.

--- Assets/Scripts/CoinBehaviour.cs
+++ Assets/Scripts/CoinBehaviour.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class CoinB : MonoBehaviour
+public class CoinBehaviour : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    private int coinValue = 1;
+    private AudioSource coinAudio; 
+    void Start() {
+        coinAudio = GetComponent<AudioSource>(); 
     }
+    
+    //Collision of player and coin
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+            CoinManager coinmanager = other.GetComponent<CoinManager>(); 
+            if(coinmanager != null) {
+                coinmanager.AddCoins(coinValue); 
+                //Play audio
+            coinAudio.Play(); 
+            }
+            
+            //Destroy coin 
+            Destroy(gameObject); 
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
